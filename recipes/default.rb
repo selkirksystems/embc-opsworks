0964dd6507b393f66600f1f503a11ae4198445e0
douglbolsen <dolsen@selkirksystems.com>
2015-06-26 10:38:43 -0700

    Reverted back to simpler code

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,24 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-#include_recipe "docker"
-group "docker"
 group "docker" do
   members "ec2-user"
 end
 
-case node[:platform]
-  when "ubuntu","debian"
-    package "docker.io" do
-      action :install
-    end
-  when 'centos','redhat','fedora','amazon'
-    package "docker" do
-      action :install
-    end
-end
-
-service "docker" do
-  action :start
-end
-
+include_recipe "docker"
